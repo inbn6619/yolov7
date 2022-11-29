c3f29f3f5e4f2500a30b18fe42516f16b0e43fb7
inbn6619 <inbn6619@gmail.com>
2022-11-29 08:19:35 +0000

    change name ByteTrack

---
+++ ByteTrack_test/yolox/tracker/Points_in_polygon.py
@@ -0,0 +1,25 @@
+import numpy as np
+from shapely.geometry import Point, Polygon
+
+
+def limited_area(bbox, area_poly):
+    makecenter = lambda x1, y1, x2, y2 : (x1 + (x2 - x1) / 2, y1 + (y2 - y1) / 2)
+    bbox_center = [makecenter(box[0], box[1], box[2], box[3]) for box in bbox]
+    checkpoint = lambda x : x.within(area_poly)
+    check = np.array([checkpoint(Point(dot)) for dot in bbox_center])
+
+    return check
+
+
+
+area = [(36, 768), (936, 816), (940, 419), (1556, 430), (1384, 279), (491, 275)]
+
+poly = Polygon(area)
+
+mealarea = [(61, 816), (22, 690), (908, 739), (913, 897)]
+
+waterarea = [(493, 277), (485, 223), (1386, 225), (1384, 277)]
+
+mealarea_poly = Polygon(mealarea)
+
+waterarea_poly = Polygon(waterarea)
